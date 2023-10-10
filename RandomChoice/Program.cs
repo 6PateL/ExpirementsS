10c7f6b7d49e8f7ee275ab7eea5f64aa54d43829
6PaTeL <jkdjkd789@gmail.com>
2023-10-10 21:06:01 +0300

    Rnd

---
+++ RandomChoice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomChoice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new RandomChoice();
+            var rnd = new Random(); 
+            random.SetExceptedNumbers( new int[] { 3, 5, 7, 8 } );
+            int result = random.GetRandomWithExceptedNumbers(1, 15);
+
+            for(int i = 0; i < 20; i++)
+            {
+                Console.Write(random.GetRandomWithExceptedNumbers(0,10) + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 20; i++)
+            { 
+                Console.Write(rnd.Next(0, 10) + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ RandomChoice/RandomChoice.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomChoice
+{
+    public class RandomChoice
+    {
+        private List<int> _exceptedNumbers = new List<int>();
+        private Random rnd = new Random(); 
+
+        public RandomChoice()
+        {
+            
+        }
+
+        public void SetExceptedNumbers(int[] numbers) 
+        {
+            _exceptedNumbers.AddRange(numbers);
+        }
+        public void SetExceptedNumber(int number)
+        {
+            _exceptedNumbers.Add(number); 
+        }
+
+        public int GetRandom(int min, int max)
+        {
+            return rnd.Next(min, max); 
+        }
+        public int GetRandomWithExceptedNumbers(int min, int max)
+        {
+            int random = rnd.Next(min, max);
+            do
+            {
+                random = rnd.Next(min, max);
+            }
+            while(_exceptedNumbers.Contains(random));
+            return random; 
+        }
+    }
+}
